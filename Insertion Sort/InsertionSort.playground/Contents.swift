506d6e8bd44e00f3d6dfe07329b9b6246791ee1e
Andy Ron <randy.njfu@gmail.com>
2018-09-17 12:25:51 +0800

    插入排序 校对

--- Insertion Sort/InsertionSort.playground/Contents.swift
+++ Insertion Sort/InsertionSort.playground/Contents.swift
@@ -1,24 +1,24 @@
-//: Playground - noun: a place where people can play
-
-// last checked with Xcode 9.0b4
-#if swift(>=4.0)
-print("Hello, Swift 4!")
-#endif
-
-func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
-  var a = array
-  for x in 1..<a.count {
-    var y = x
-    let temp = a[y]
-    while y > 0 && isOrderedBefore(temp, a[y - 1]) {
-      a[y] = a[y - 1]
-      y -= 1
+/*
+ 插入排序
+ */
+public func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+    var a = array
+    for x in 1..<a.count {
+        var y = x
+        let temp = a[y]
+        while y > 0 && isOrderedBefore(temp, a[y-1]) {
+            a[y] = a[y-1]
+            y -= 1
+        }
+        a[y] = temp
     }
-    a[y] = temp
-  }
-  return a
+    return a
 }
 
-let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
-insertionSort(list, <)
-insertionSort(list, >)
+
+let numbers = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+insertionSort(numbers, <)
+insertionSort(numbers, >)
+
+let strings = [ "b", "a", "d", "c", "e" ]
+insertionSort(strings, <)
